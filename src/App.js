0a0b52f33a0d62e1f88db226e8eb21573162cc58
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-12-21 18:21:25 +0545

    Added SEGA Font to title

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
     });
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchField onSearchChange={this.onSearchChange} />
         <CardList robots={filteredRobot} />
       </div>
